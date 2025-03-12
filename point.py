3dc99b3c67cfc87a0f5c16d100a687961bb685ed
amedeocroci <amedeo.croci@gmail.com>
2025-03-12 17:44:38 +0100

    first

--- point.py
+++ point.py
@@ -1,3 +1,5 @@
+import random  # Correct import
+
 class Point:
     def __init__(self, x, y):
         """
@@ -5,22 +7,34 @@ class Point:
         :param x: the x position on the axis
         :param y: the y position on the axis
         """
-        self.x = x #define x attribute via self.x
-        self.y = y # and assign the value x to it
+        self.x = x  # Define x attribute via self.x
+        self.y = y  # Define y attribute via self.y
 
     def __str__(self):
         """
         Magic method that is called when we try to print an instance
         :return: <x,y>
         """
-        return f"p({self,x}, {self.y})"
+        return f"p({self.x}, {self.y})"
 
-#now we need to instantiate it!
-p= Point(1, 2) #p is an instance of 1 and 2
-p2=Point(2, 3)
-p4= Point(4.4, -55)
+# Now we need to instantiate it!
+p = Point(1, 2)
+p2 = Point(2, 3)
+p4 = Point(4.4, -55)
+
 print(f"p.x={p.x} and p.y={p.y}")
 print(f"p4.x={p4.x} and p4.y={p4.y}")
-p.x=20
+
+p.x = 20
 print(f"p.x={p.x} and p.y={p.y}")
 print(p)
+
+# Create a list of 5 random points
+points = []
+for i in range(5):
+    points.append(Point(random.randint(-10, 10),
+                        random.randint(-10, 10)))
+
+print("I got these 5 random points:")
+for p in points:
+    print(p)
