ba63d4d31795a61b530e3efbbad50226523be4ae
amedeocroci <amedeo.croci@gmail.com>
2025-03-19 18:04:44 +0100

    committing on wednesday

--- color point.py
+++ color point.py
@@ -1,34 +1,36 @@
 from point import Point
+import random
 
-class ColorPoint:
-    def _innit_(self,x,y,color):
-        #raise and exception if we try to not have a number
-        if not isinstance(x, (int,float)):
-            raise PointException("x must be a number")
-        if not isinstance(x, (int,float)):
-            raise PointException("x must be a number")
+class PointException(Exception):
+    pass
 
-        super()._init_(x, y) # this replaces the self.x and self. y
+class ColorPoint(Point):
+    def __init__(self, x, y, color):
+        # raise an exception if we try to have not a number
+        if not isinstance(x, (int, float)):
+            raise PointException("x must be an number")
+        if not isinstance(y, (int, float)):
+            raise PointException("y must be an number")
+
+        super().__init__(x, y) # this replaces the self.x and self.y
         self.color = color
 
-
-    def _str_(self):
+    def __str__(self):
         return f"<{self.color}: {self.x}, {self.y}>"
 
-p = ColorPoint(1,2, "red")
-print(p.distance_orig())
-print(p)
-
-colors = ["red", "green", "blue", "yellow", "orange", "white", "purple"]
-
-color_points = []
-for i in range(10):
-    color_points.append(
-        ColorPoint(random.randint(-10,10),
-                   random.randint(-10, 10),
-                   random.choice(colors)))
-
-print(color_points)
-color_points.sort()
-print(color_points)
-#committing
+if __name__ == "__main__":
+    p = ColorPoint(1, 2, "red")
+    print(p.distance_orig())
+    print(p)
+    # colors = ["red", "green", "blue", "yellow", "black", "magenta",
+    #           "cyan", "white", "burgundy", "periwinkle", "marsala"]
+    # color_points = []
+    # for i in range(10):
+    #     color_points.append(
+    #         ColorPoint(random.randint(-10, 10),
+    #                    random.randint(-10, 10),
+    #                    random.choice(colors)))
+    #
+    # print(color_points)
+    # color_points.sort()
+    # print(color_points)
